ba72aa183bb4738eece05f07efd01d374da1b692
Edward Blakeley <blakeleyjr@berkeley.edu>
2014-03-07 22:29:34 -0800

    partially finished bag

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -9,17 +9,24 @@ $('#say_hello').click(function() {
 
 // Problem 2 (Houdini) ------------------------------------------------------
 $('#disappear').click(function() {
+	$('#houdini_text').hide()
   //WRITE CODE HERE
 });
 
 $('#reappear').click(function() {
+	$('#houdini_text').show()
   //WRITE CODE HERE
 });
 
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
-// WRITE CODE HERE
+$('#pink').click(function() {
+	$('#tickled_text').css('color', 'red');
+});
 
 
 // Problem 4 (Greet Me) -----------------------------------------------------
 // WRITE CODE HERE
+$('#greet').click(function() {
+	$('#my_name').val()
+});
